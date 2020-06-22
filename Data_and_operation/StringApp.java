9b0c489086defae937df3a775a59e293d192db2e
MeotJH <66897806+MeotJH@users.noreply.github.com>
2020-06-22 20:25:45 +0900

    자바 문자와 숫자 사용법

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character 작은따옴표 안쓰기
+		System.out.println("H"); // String
+		
+		System.out.println("Hello "
+				+ "World"); // 이클립스 기능
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		// Hello "World" Escape 역슬래쉬로 가능
+		System.out.println("Hello \"World\"");
+	}
+
+}
